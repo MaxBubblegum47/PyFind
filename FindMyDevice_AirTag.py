538893ef7e7a82b2a5eafa2ad9a27997c95ddf34
MaxBubblegum47 <lorenzostigliano@gmail.com>
2023-09-01 17:57:26 +0200

    Removing redundant code

--- FindMyDevice_AirTag.py
+++ FindMyDevice_AirTag.py
@@ -2,19 +2,11 @@
 # PAIRING WITH AIRTAG #
 #######################
 
+from utility import *
 from tinyec import registry
 import secrets
 
-# STEP 1: compute ECDH
-
-'''Forse ho capito: in questa prima fase praticamente Alice sarebbe come se fosse il telefono smarrito
-e io Bob, cioe' il proprietario. Ci stiamo scambiando un segreto comune che poi mi serviera' per andare
-a decifrare il contenuto cifrato di iCloud'''
-
-# the idea is that (from Pratical Cryptography for Developers by Svetlin Nakov, seems possible to zip the two coordinates of a point in one single hex number)
-def compress(pubKey):
-    return hex(pubKey.x) + hex(pubKey.y % 2)[2:]
-
+# STEP 1: compute ECDH with the AirTag
 print("Pairing between my iPhone and Lost Device...")
 
 # choosing the curve
@@ -41,11 +33,6 @@ print("Check if the iPhone and the Lost Device shared the same shared key:", iPh
 ########################
 # LOST DEVICE ACTIVITY #
 ########################
-
-import os
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.x963kdf import X963KDF
-
 # algorithm steps
 # 1. SKi = KDF(SKi-1, update, 32)
 # 2. (ui, vi) = KDF(SKi, diversify, 72)
@@ -132,48 +119,12 @@ AES_GCM_KEY_TO_SPLIT = xkdf.derive(bytes(compress(FounderSharedKey), 'ISO-8859-1
 e = AES_GCM_KEY_TO_SPLIT[:16]
 IV = AES_GCM_KEY_TO_SPLIT[16:32]
 
-import base64
-import hashlib
-
-from Cryptodome.Cipher import AES  # from pycryptodomex v-3.10.4
-from Cryptodome.Random import get_random_bytes
-
-HASH_NAME = "SHA512"
-IV_LENGTH = 16
-ITERATION_COUNT = 65535
-KEY_LENGTH = 16
-SALT_LENGTH = 16
-TAG_LENGTH = 16
-
-
-def encrypt(password, plain_message):
-    salt = get_random_bytes(SALT_LENGTH) 
-    iv = IV
-
-    secret = get_secret_key(password, salt)
-
-    cipher = AES.new(secret, AES.MODE_GCM, iv)
-
-    encrypted_message_byte, tag = cipher.encrypt_and_digest(
-        plain_message.encode("utf-8")
-    )
-    cipher_byte = salt + iv + encrypted_message_byte + tag
-
-    encoded_cipher_byte = base64.b64encode(cipher_byte)
-    return bytes.decode(encoded_cipher_byte)
-
-
-def get_secret_key(password, salt):
-    return hashlib.pbkdf2_hmac(
-        HASH_NAME, password.encode(), salt, ITERATION_COUNT, KEY_LENGTH
-    )
-
 outputFormat = "{:<25}:{}"
 secret_key = str(e)
 plain_text = "Your_plain_text"
 
 print("------ AES-GCM Encryption ------")
-cipher_text = encrypt(secret_key, plain_text)
+cipher_text = encrypt(secret_key, plain_text, IV)
 print(outputFormat.format("encryption input", plain_text))
 print(outputFormat.format("encryption output", cipher_text))
 
@@ -202,23 +153,8 @@ final_key_TO_SPLIT = xkdf.derive(bytes(compress(final_key), 'ISO-8859-1'))
 e2 = final_key_TO_SPLIT[:16]
 IV2 = final_key_TO_SPLIT[16:32]
 
-def decrypt(password, cipher_message):
-    decoded_cipher_byte = base64.b64decode(cipher_message)
-
-    salt = decoded_cipher_byte[:SALT_LENGTH]
-    iv = IV2
-    encrypted_message_byte = decoded_cipher_byte[
-        (IV_LENGTH + SALT_LENGTH) : -TAG_LENGTH
-    ]
-    tag = decoded_cipher_byte[-TAG_LENGTH:]
-    secret = get_secret_key(password, salt)
-    cipher = AES.new(secret, AES.MODE_GCM, iv)
-
-    decrypted_message_byte = cipher.decrypt_and_verify(encrypted_message_byte, tag)
-    return decrypted_message_byte.decode("utf-8")
-
 secret_key = str(e2)
-decrypted_text = decrypt(secret_key, cipher_text)
+decrypted_text = decrypt(secret_key, cipher_text, IV2)
 
 print("\n------ AES-GCM Decryption ------")
 print(outputFormat.format("decryption input", cipher_text))
